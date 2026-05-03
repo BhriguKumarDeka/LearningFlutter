1cf4e63d5d4a4dbe87653d2faf311ff8f147d88a
BhriguKD <vrigukumar710@gmail.com>
2026-05-04 01:20:25 +0530

    Organize Dart code with packages and libraries
    - Created a resuable Dart package
    - Used exports to define a public API
    - Added local path dependencies

---
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -0,0 +1,8 @@
+class CommandRunner {
+  /// Runs the command-line application logic with the given arguments.
+  Future<void> run(List<String> input) async {
+    print('CommandRunner received arguments: $input');
+  }
+}
+
+
